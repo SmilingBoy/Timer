ee6ea21a470d5919a37f5f3ed1ac50210d768626
SmilingBoy <lx@macbook-2.local>
2016-05-13 09:51:02 +0800

    Initial Commit

---
+++ Timer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Timer
+//
+//  Created by lx on 16/5/13.
+//  Copyright © 2016年 LiZhiqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Timer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Timer
+//
+//  Created by lx on 16/5/13.
+//  Copyright © 2016年 LiZhiqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
